9983c4d9828802f5a1946be3afed2eff8420b88b
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2024-04-21 23:59:13 +0530

    Update HP-left.cpp

--- Patterns/HP-left.cpp
+++ Patterns/HP-left.cpp
@@ -1,16 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int i , j , height ;
-    cout<<"Enter The value of Height: \n";
-    cin>> height;
+int main()
+{
+    int i, j, height;
+    cout << "Enter The value of Height: \n";
+    cin >> height;
 
-    for(i=height;i>=0;i--){
-        for(j=0;j<=i;j++){
-            cout<<"* ";
+    for (i = height; i >= 0; i--)
+    {
+        for (j = 0; j <= i; j++)
+        {
+            cout << "* ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
     return 0;
 }
